7f4b22461bff9684cf757fec734aae8d0e0ea300
János Székely <szekelyjani@gmail.com>
2024-10-28 16:58:22 +0100

    Initial Commit

---
+++ RepoWatcher/RepoWatcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  RepoWatcherApp.swift
+//  RepoWatcher
+//
+//  Created by Szekely Janos on 2024. 10. 28..
+//
+
+import SwiftUI
+
+@main
+struct RepoWatcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
